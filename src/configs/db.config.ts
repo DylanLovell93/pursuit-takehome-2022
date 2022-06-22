eb97deca8484aacea39baf25effe3a15830be898
DylanLovell <arcanyne7@gmail.com>
2022-06-22 18:34:36 -0400

    server changes for local testing

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,24 +1,19 @@
 import knex from 'knex';
 
+const { HOST, PPORT, DATABASE, USER, PASSWORD = '' } = process.env;
+
 export const pgKnex = knex({
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false }
-        }
-        : {
-            host: 'db',
-            port: 5432,
-            database: 'postgres',
-            user: 'pursuit',
-            password: 'pursuit2022',
-        }
-
-})
-
-
-
-
-
-
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: HOST,
+        port: Number(PPORT),
+        database: DATABASE,
+        user: USER,
+        password: PASSWORD,
+      },
+});
